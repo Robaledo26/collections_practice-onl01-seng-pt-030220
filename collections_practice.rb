a0b1832e432242c77d80744df76eb85e098cd42e
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-15 20:56:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+
+def sort_array_desc (array)
+  array.sort {|x,y| y <=> x }
+end
+ 
+ def swap_elements (array)
+   array[1], array[2] =array[2], array[1]
+   return array 
+  end
+end
+
+def reverse.array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.collect do |string|
+    string|2| = "2"
+  return string 
+ end
+end
 
